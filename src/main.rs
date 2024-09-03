eb7248bc9adaf23e97680c1b1b277ae65ed8886b
fricht <fricht@users.noreply.github.com>
2024-09-03 21:58:25 +0200

    Working version, just the backend for now. There is a test in the main function.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
+use std::fs;
 mod slide_creator;
 
-
 fn main() {
-    println!("Hello, world!");
+    let test_slide = slide_creator::SlidePresentation {
+        slides: vec![
+            slide_creator::SlideData {
+                name: "Hello World!".to_string(),
+                notes: "Nothing to say here".to_string(),
+                items: vec![slide_creator::ItemData {
+                    id: "svg1".to_string(),
+                    transition: "0.5s ease-in-out".to_string(),
+                    opacity: 1.,
+                    position: [0., 0.],
+                    rotation: 0.,
+                    scale: 10.,
+                }],
+            },
+            slide_creator::SlideData {
+                name: "Slide 2".to_string(),
+                notes: "Nothing to say here, again".to_string(),
+                items: vec![slide_creator::ItemData {
+                    id: "svg1".to_string(),
+                    transition: "1s ease-in-out".to_string(),
+                    opacity: 0.8,
+                    position: [-50., -50.],
+                    rotation: 0.,
+                    scale: 50.,
+                }],
+            },
+        ],
+    };
+    let test_svg = slide_creator::SVG {
+        data: "<svg class=\"svg-layer\" viewBox=\"0 0 100 100\"><circle cx=\"50\" cy=\"50\" r=\"45\" stroke=\"black\" stroke-width=\"3\" fill=\"red\" /></svg>".to_string(),
+        id: "svg1".to_string(),
+    };
+    match slide_creator::create_web(vec![test_svg], test_slide) {
+        Ok(html) => {
+            if let Err(e) = fs::write("output-presentation.html", html) {
+                panic!("{}", e)
+            }
+        }
+        Err(e) => panic!("{}", e),
+    }
 }
--- src/slide_creator.rs
+++ src/slide_creator.rs
@@ -1,16 +1,19 @@
 use serde::Serialize;
+use serde_json;
 
+const HTML_TEMPLATE: &str = include_str!("ressources/mainpage.html");
+const BREAK_CHAR: &str = "¿";
 
 #[derive(Serialize)]
 pub struct SlidePresentation {
-    pub slides: Vec<SlideData>
+    pub slides: Vec<SlideData>,
 }
 
 #[derive(Serialize)]
 pub struct SlideData {
     pub name: String,
     pub notes: String,
-    pub items: Vec<ItemData>
+    pub items: Vec<ItemData>,
 }
 
 #[derive(Serialize)]
@@ -20,5 +23,41 @@ pub struct ItemData {
     pub opacity: f64,
     pub position: [f64; 2],
     pub rotation: f64,
-    pub scale: f64
+    pub scale: f64,
+}
+
+pub struct SVG {
+    pub data: String,
+    pub id: String,
+}
+
+fn svg_to_str(svg: &SVG) -> String {
+    let mut data = "<div class=\"svg-container\"  id=\"".to_string();
+    data.push_str(&svg.id);
+    data.push_str("\">");
+    data.push_str(&svg.data);
+    data.push_str("</div>");
+    data
+}
+
+fn svgs_to_str(svgs: &Vec<SVG>) -> String {
+    let mut data = "".to_string();
+    for svg in svgs {
+        data.push_str(&svg_to_str(&svg));
+    }
+    data
+}
+
+pub fn create_web(svgs: Vec<SVG>, slides: SlidePresentation) -> Result<String, String> {
+    let slides_data = serde_json::to_string(&slides).map_err(|e| e.to_string())?;
+    let html_page_template: Vec<&str> = HTML_TEMPLATE.split(BREAK_CHAR).collect();
+    if html_page_template.len() != 3 {
+        return Err("Failed to load template.".to_string());
+    }
+    let mut html_page = html_page_template[0].to_string();
+    html_page.push_str(&svgs_to_str(&svgs));
+    html_page.push_str(html_page_template[1]);
+    html_page.push_str(&slides_data);
+    html_page.push_str(html_page_template[2]);
+    Ok(html_page)
 }
